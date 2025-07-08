f3981ce1726da04e9cc76c8cb3acf0dd368c7399
Iryna Rohachova <iamrohachova@gmail.com>
2025-07-08 21:36:14 +0200

    removed the first number in print

--- Lesson2.py
+++ Lesson2.py
@@ -9,4 +9,9 @@ print(user_input // 1 % 10) #1234/1=1234 1234/10=123.4 answer=4
 
 #task 2.2
 user_input = int(input("Enter 5-digit number: ")) #12345
-print(str(user_input // 1 % 10) + str(user_input // 10 % 10) + str(user_input // 100 % 10) + str(user_input // 1000 % 10) + str(user_input // 10000 % 10))
+number_one = (user_input // 1 % 10)
+number_two = (user_input // 10 % 10)
+number_three = (user_input // 100 % 10)
+number_four = (user_input // 1000 % 10)
+number_five = (user_input // 10000 % 10)
+print(int(number_one), int(number_two),  int(number_three),  int(number_four), int(number_five))
