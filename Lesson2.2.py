90e0816f9cfe7b68828d4f7ffe5d526fe5372a13
Iryna Rohachova <iamrohachova@gmail.com>
2025-07-10 21:17:02 +0200

    changed print value

--- Lesson2.2.py
+++ Lesson2.2.py
@@ -5,4 +5,4 @@ number_two = (user_input // 10 % 10) #4
 number_three = (user_input // 100 % 10) #3
 number_four = (user_input // 1000 % 10) #2
 number_five = (user_input // 10000 % 10) #1
-print(number_one * 10000 + number_two * 1000 + number_three * 100 + number_four * 10 + number_five)
+print(number_one * 10000 + number_two * 1000 + number_three * 100 + number_four * 10 + number_five) #54321
