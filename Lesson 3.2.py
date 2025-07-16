2f5ce307340bdf2d050b94210c9e405bd7e0cb51
Iryna Rohachova <iamrohachova@gmail.com>
2025-07-16 12:20:36 +0300

    Lesson 3.2

--- Lesson 3.2.py
+++ Lesson 3.2.py
@@ -4,8 +4,7 @@ lst = [12, 3, 4, 10]
 # lst = [12, 3, 4, 10, 8]
 
 if len(lst) > 1:
-    last_number = lst[-1]
-    lst.pop(-1)
+    last_number = lst.pop(-1)
     lst.insert(0,last_number)
     print(lst)
 else:
