f1beff816c201a7963631b7423aae6945c3e19e7
Iryna Rohachova <iamrohachova@gmail.com>
2025-07-21 00:45:12 +0200

    lesson 4.1 is updated again :)

--- Lesson 4/Lesson 4.1.py
+++ Lesson 4/Lesson 4.1.py
@@ -4,11 +4,14 @@ lst = [0, 1, 0, 12, 3] # -> [1, 12, 3, 0, 0]
 # [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0] -> [9, 7, 31, 45, 45, 45, 96, 0, 0, 0, 0, 0, 0, 0]
 n = 0
 
-for _ in range(len(lst)):
-    if lst[_] != 0:
-        n += 1
-        print(lst[_])
-for _ in range(len(lst)):
-    if lst[_] == 0:
-        n += 1
-        print(lst[_])
+if len(lst) == 1:
+    print(lst)
+if len(lst) > 1:
+    for _ in range(len(lst)):
+        if lst[_] != 0:
+            n += 1
+            print(lst[_])
+    for _ in range(len(lst)):
+        if lst[_] == 0:
+            n += 1
+            print(lst[_])
