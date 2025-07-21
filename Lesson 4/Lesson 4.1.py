37684e1eb3f65953e8ec2520990910ca2cf461a7
Iryna Rohachova <iamrohachova@gmail.com>
2025-07-21 12:29:37 +0200

    lessons 4.1 and 4.2 is updated

--- Lesson 4/Lesson 4.1.py
+++ Lesson 4/Lesson 4.1.py
@@ -2,16 +2,7 @@ lst = [0, 1, 0, 12, 3] # -> [1, 12, 3, 0, 0]
 # [0] -> [0]
 # [1, 0, 13, 0, 0, 0, 5] -> [1, 13, 5, 0, 0, 0, 0]
 # [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0] -> [9, 7, 31, 45, 45, 45, 96, 0, 0, 0, 0, 0, 0, 0]
-n = 0
 
-if len(lst) == 1:
-    print(lst)
-if len(lst) > 1:
-    for _ in range(len(lst)):
-        if lst[_] != 0:
-            n += 1
-            print(lst[_])
-    for _ in range(len(lst)):
-        if lst[_] == 0:
-            n += 1
-            print(lst[_])
+not_zero_lst = [x for x in lst if x != 0]
+not_zero_lst.extend([0] * (len(lst) - len(not_zero_lst)))
+print(not_zero_lst)
---
+++ Lesson 4/Lesson 4.2.py
@@ -0,0 +1,16 @@
+lst = [0, 1, 7, 2, 4, 8] # => (0 + 7 + 4) * 8 = 88
+# [1, 3, 5] => 30
+# [6] => 36
+# [] => 0
+
+if len(lst) == 0:
+    print(0)
+
+if len(lst) >= 1:
+    last_number = lst[-1]
+
+    sum_numbers = 0
+
+    for i in range(0, len(lst), 2):
+        sum_numbers += lst[i]
+    print(sum_numbers * last_number)
