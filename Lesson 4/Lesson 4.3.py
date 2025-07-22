a4f369d3058a38aa4bda61ec1279afc46b5d9fa8
Iryna Rohachova <iamrohachova@gmail.com>
2025-07-22 11:51:23 +0200

    lesson 4.3 updated

--- Lesson 4/Lesson 4.3.py
+++ Lesson 4/Lesson 4.3.py
@@ -3,7 +3,6 @@ import random
 lst_length = random.randint(3,10)
 
 random_lst = [random.randint(1,10) for element in range(lst_length)]
-
 print(random_lst)
 
 new_list = [random_lst[0], random_lst[2], random_lst[-2]]
