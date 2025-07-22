a40be28beb9da587b54f938ec09b40dded5b0e53
Iryna Rohachova <iamrohachova@gmail.com>
2025-07-22 14:57:10 +0200

    lesson 4.1 updated

--- Lesson 4/Lesson 4.1.py
+++ Lesson 4/Lesson 4.1.py
@@ -3,6 +3,20 @@ lst = [0, 1, 0, 12, 3] # -> [1, 12, 3, 0, 0]
 # [1, 0, 13, 0, 0, 0, 5] -> [1, 13, 5, 0, 0, 0, 0]
 # [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0] -> [9, 7, 31, 45, 45, 45, 96, 0, 0, 0, 0, 0, 0, 0]
 
-new_lst = [element for element in lst if element != 0]
-new_lst.extend([0] * (len(lst) - len(new_lst)))
-print(new_lst)
+if len(lst) == 0:
+    print(0)
+
+if len(lst) >= 1:
+
+    position = 0
+
+    for not_zero_number in lst:
+        if not_zero_number != 0:
+            lst[position] = not_zero_number
+            position += 1
+            # print(not_zero_number)
+
+    for zero in range(position, len(lst)):
+        lst[zero] = 0
+
+    print(lst)
