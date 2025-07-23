148466650726239d90be9ba30eff6875c1c12ed2
Iryna Rohachova <iamrohachova@gmail.com>
2025-07-23 13:57:40 +0200

    lesson 5.3 added

---
+++ Lesson 5/Lesson 5.3.py
@@ -0,0 +1,17 @@
+# 'Python Community' -> #PythonCommunity
+# 'i like python community!' -> #ILikePythonCommunity
+# 'Should, I. subscribe? Yes!' -> #ShouldISubscribeYes
+
+text_input = input("Enter text for a hashtag: ")
+
+hashtag = (text_input.title())
+
+punctuation_symbols = '''!"#$%&'()*+,-./:;<=>?@[]^_`{|}~'''
+
+for symbol in punctuation_symbols:
+    hashtag = hashtag.replace(symbol, "").replace(" ", "")
+
+if len(hashtag) >=140:
+    print("#" +  hashtag[:139])
+else:
+    print("#" + hashtag)
