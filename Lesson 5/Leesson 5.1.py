296c0bc59537f26359d951265872f0bfc082ddb1
Iryna Rohachova <iamrohachova@gmail.com>
2025-07-30 14:18:17 +0200

    5.1 push

---
+++ Lesson 5/Leesson 5.1.py
@@ -0,0 +1,21 @@
+import keyword
+import string
+
+user_name = input("Add your name here: ")
+allowed_characters = set(string.ascii_letters + string.digits + "_")
+upper_case_letters = set(string.ascii_uppercase)
+
+if not user_name:
+    print(False)
+elif user_name[0].isdigit():
+    print(False)
+elif any(ch not in allowed_characters for ch in user_name):
+    print(False)
+elif any(ch in upper_case_letters for ch in user_name):
+    print(False)
+elif user_name.count("_") > 1:
+    print(False)
+elif user_name in keyword.kwlist:
+    print(False)
+else:
+    print(True)
