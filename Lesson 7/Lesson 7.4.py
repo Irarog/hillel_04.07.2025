5b2df7c6f6eb6cb1512da6e997efb83b1c4a4403
Iryna Rohachova <iamrohachova@gmail.com>
2025-08-01 15:37:37 +0200

    7.4 added

---
+++ Lesson 7/Lesson 7.4.py
@@ -0,0 +1,7 @@
+def common_elements():
+    set_3 = set(el for el in range(0,100) if el % 3 == 0)
+    set_5 = set(el for el in range(0,100) if el % 5 == 0)
+    return set_3.intersection(set_5)
+
+assert common_elements() == {0, 75, 45, 15, 90, 60, 30}
+print(common_elements())
