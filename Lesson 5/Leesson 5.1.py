ef3f13c2f67763a9e9f04af1cf3b03a652f68eb7
Iryna Rohachova <iamrohachova@gmail.com>
2025-08-01 17:59:09 +0200

    5.1 updated

--- Lesson 5/Leesson 5.1.py
+++ Lesson 5/Leesson 5.1.py
@@ -7,13 +7,15 @@ upper_case_letters = set(string.ascii_uppercase)
 
 if not user_name:
     print(False)
+# elif user_name in allowed_characters and user_name.count("_") > 1:
+#     print(True)
 elif user_name[0].isdigit():
     print(False)
 elif any(ch not in allowed_characters for ch in user_name):
     print(False)
 elif any(ch in upper_case_letters for ch in user_name):
     print(False)
-elif user_name.count("_") > 1:
+elif user_name.count("_") > 1 and not allowed_characters:
     print(False)
 elif user_name in keyword.kwlist:
     print(False)
