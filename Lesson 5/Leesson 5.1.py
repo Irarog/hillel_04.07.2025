9461feb0092a30854bd11685c650d50604f54b17
Iryna Rohachova <iamrohachova@gmail.com>
2025-08-01 18:14:29 +0200

    5.1 updated

--- Lesson 5/Leesson 5.1.py
+++ Lesson 5/Leesson 5.1.py
@@ -2,18 +2,19 @@ import keyword
 import string
 
 user_name = input("Add your name here: ")
+punctuation = set(string.punctuation)
 allowed_characters = set(string.ascii_letters + string.digits + "_")
 upper_case_letters = set(string.ascii_uppercase)
 
 if not user_name:
     print(False)
-# elif user_name in allowed_characters and user_name.count("_") > 1:
-#     print(True)
 elif user_name[0].isdigit():
     print(False)
 elif any(ch not in allowed_characters for ch in user_name):
     print(False)
 elif any(ch in upper_case_letters for ch in user_name):
+    print(False)
+elif all(ch in punctuation for ch in user_name):
     print(False)
 elif user_name.count("_") > 1 and not allowed_characters:
     print(False)
