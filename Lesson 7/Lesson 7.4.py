203175b6593dc4b6ac3e16fcaa54f2fcbe1ff545
Iryna Rohachova <iamrohachova@gmail.com>
2025-08-01 15:45:30 +0200

    7.4 updated

--- Lesson 7/Lesson 7.4.py
+++ Lesson 7/Lesson 7.4.py
@@ -1,4 +1,4 @@
-def common_elements():
+def common_elements() -> set:
     set_3 = set(el for el in range(0,100) if el % 3 == 0)
     set_5 = set(el for el in range(0,100) if el % 5 == 0)
     return set_3.intersection(set_5)
