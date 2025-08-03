8c986c2f342739b28e2e393827412ecbc17542f9
Iryna Rohachova <iamrohachova@gmail.com>
2025-08-03 19:07:05 +0200

    5.1 updated

--- Lesson 5/Leesson 5.1.py
+++ Lesson 5/Leesson 5.1.py
@@ -14,9 +14,9 @@ elif any(ch not in allowed_characters for ch in user_name):
     print(False)
 elif any(ch in upper_case_letters for ch in user_name):
     print(False)
+elif user_name.count("_") <= 1:
+    print(True)
 elif all(ch in punctuation for ch in user_name):
-    print(False)
-elif user_name.count("_") > 1 and not allowed_characters:
     print(False)
 elif user_name in keyword.kwlist:
     print(False)
