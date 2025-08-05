65da84a791cb0cff836c7e3ae6adee816c0faf83
Iryna Rohachova <iamrohachova@gmail.com>
2025-08-05 12:33:37 +0200

    8.2 updated

---
+++ Lesson 8/Lesson 8.2.py
@@ -0,0 +1,15 @@
+import string
+def is_palindrome(text_input:str) -> bool:
+    text_input = input("Add some text: ").lower()
+    text_input_join = "".join(ch for ch in text_input if ch not in string.punctuation).replace(" ", "")
+    text_input_reverse = text_input_join[::-1]
+    if text_input_join == text_input_reverse:
+        return True
+    else:
+        return False
+
+assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1'
+assert is_palindrome('0P') == False, 'Test2'
+assert is_palindrome('a.') == True, 'Test3'
+assert is_palindrome('aurora') == False, 'Test4'
+print("ОК")
