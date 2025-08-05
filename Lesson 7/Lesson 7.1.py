b66b05443f0faf9aa6afba62b446a8a256068d7c
Iryna Rohachova <iamrohachova@gmail.com>
2025-08-05 13:09:26 +0200

    7.1 updated

--- Lesson 7/Lesson 7.1.py
+++ Lesson 7/Lesson 7.1.py
@@ -1,12 +1,9 @@
-def say_hi(name_input, age_input):
-
-    name_input = str(input("What is your name?: "))
-    age_input = int(input("What is your age?: "))
-
-    if name_input and age_input > 0:
-        return f"Hi. My name is {name_input} and I'm {age_input} years old"
+from typing import Union
+def say_hi(name:str, age:int) -> Union[str, None]:
+    if name and age > 0:
+        return f"Hi. My name is {name} and I'm {age} years old"
     else:
-        return "Error"
+        return None
 
 assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Test1'
 assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", 'Test2'
