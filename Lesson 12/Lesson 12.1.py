1162d03bb0de7ed724772a6d6d8c07fd30f984f7
Iryna Rohachova <iamrohachova@gmail.com>
2025-08-28 21:35:24 +0200

    12.1 updated

---
+++ Lesson 12/Lesson 12.1.py
@@ -0,0 +1,14 @@
+import codecs
+import re
+
+
+def delete_html_tags(html_file, result_file):
+    with codecs.open(html_file, 'r', 'utf-8') as file:
+        html = file.read()
+
+        cleaned_text = re.sub(r'<.*?>', '\n', html, flags=re.DOTALL)
+
+    with codecs.open(result_file, 'w', 'utf-8') as cleaned_file:
+        cleaned_file.write(cleaned_text)
+
+delete_html_tags('draft.html', 'cleaned.txt')
