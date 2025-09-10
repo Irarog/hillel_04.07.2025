535f437f74395ea2e6f00a172b43b3bb12af105b
Iryna Rohachova <iamrohachova@gmail.com>
2025-09-10 21:34:34 +0300

    13.1 updated

--- Lesson 13/Lesson 13.1.py
+++ Lesson 13/Lesson 13.1.py
@@ -1,36 +1,36 @@
 class Human:
 
-    def __init__(self, gender, age, first_name, last_name):
+    def __init__(self, gender: str, age: int, first_name: str, last_name: str) -> None:
         self.gender = gender
         self.age = age
         self.first_name = first_name
         self.last_name = last_name
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'{self.gender} {self.age} {self.first_name} {self.last_name}'
 
 class Student(Human):
 
-    def __init__(self, gender, age, first_name, last_name, record_book):
+    def __init__(self, gender: str, age: int, first_name: str, last_name: str, record_book: str) -> None:
         self.gender = gender
         self.age = age
         self.first_name = first_name
         self.last_name = last_name
         self.record_book = record_book
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'{self.gender} {self.age} {self.first_name} {self.last_name} {self.record_book}'
 
 class Group(Student):
 
-    def __init__(self, number):
+    def __init__(self, number:int) -> None:
         self.number = number
         self.group = set()
 
-    def add_student(self, student):
+    def add_student(self, student: 'Student') -> None:
         self.group.add(student)
 
-    def delete_student(self, last_name):
+    def delete_student(self, last_name: str) -> None:
         student_to_remove = None
         for student in self.group:
             if student.last_name == last_name:
@@ -39,16 +39,17 @@ class Group(Student):
         if student_to_remove:
             self.group.remove(student_to_remove)
 
-    def find_student(self, last_name):
+    def find_student(self, last_name:str) -> 'Student':
         for student in self.group:
             if student.last_name == last_name:
                 return student
         return None
 
-    def __str__(self):
+    def __str__(self) -> str:
         for student in self.group:
             all_students = f'{student.first_name} {student.last_name} ({student.record_book})\n'
         return f'Number: {self.number}\n{all_students}'
+
 
 st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
 st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
