03b00f7b3cc9521d5e046c88c7425e99d4c4f836
Iryna Rohachova <iamrohachova@gmail.com>
2025-09-10 21:53:26 +0300

    6.2 updated

--- Lesson 6/Lesson 6.2.py
+++ Lesson 6/Lesson 6.2.py
@@ -1,4 +1,4 @@
-seconds_input = int(input("Enter seconds, 0 >= seconds < 8640000: "))
+seconds_input = int(input("Enter seconds, 0 <= seconds < 8640000: "))
 
 seconds_in_minute = 60
 seconds_in_hour = 60 * seconds_in_minute
@@ -10,11 +10,13 @@ hours, remainder = divmod(remainder, seconds_in_hour)
 minutes, seconds = divmod(remainder, seconds_in_minute)
 
 #function for days word
-def day_word(day):
+def day_word(day: int) -> str:
     if day == 1:
-        return "day"
+        return 'день'
+    elif 2 <= day <= 4:
+        return 'дні'
     else:
-        return "days"
+        return 'днів'
 
 # print days, hrs, min and sec
 print(f"{days} {day_word(days)}, {str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}")
