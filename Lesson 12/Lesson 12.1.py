fa908189e4411470fcfee31b6a46116b167d2143
Iryna Rohachova <iamrohachova@gmail.com>
2025-09-10 21:42:48 +0300

    12.1 updated

--- Lesson 12/Lesson 12.1.py
+++ Lesson 12/Lesson 12.1.py
@@ -2,7 +2,7 @@ import codecs
 import re
 
 
-def delete_html_tags(html_file, result_file):
+def delete_html_tags(html_file: str, result_file: str) -> None:
     with codecs.open(html_file, 'r', 'utf-8') as file:
         html = file.read()
 
